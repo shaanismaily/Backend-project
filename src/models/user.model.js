2df2488188100f8fab90a82b820e69d08dd8f856
Muhammad Shaan <shaanrazaismaily@gmail.com>
2026-06-17 17:14:38 +0530

    Add User and Video models

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,5 @@
 import mongoose, { Schema } from "mongoose";
-import jwt from "jsonwebtoken"
+import jwt from 'jsonwebtoken';
 import bcrypt from "bcrypt"
 
 const userSchema = new Schema({
@@ -54,5 +54,35 @@ userSchema.pre("save", async function (next) {
     this.password = bcrypt.hash(this.password, 10)
     next()
 })
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 export const User = mongoose.model("User", userSchema);
--- src/models/video.model.js
+++ src/models/video.model.js
@@ -36,4 +36,6 @@ const videoSchema = new Schema({
     }
 }, {timestamps: true});
 
+videoSchema.plugin(mongooseAggregatePaginate)
+
 export const Video = mongoose.model("Video", videoSchema);
