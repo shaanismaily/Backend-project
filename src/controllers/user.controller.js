5486bd0c464547a7a51e14d3ad5953ab790e0113
Muhammad Shaan <shaanrazaismaily@gmail.com>
2026-06-29 18:55:38 +0530

    Fix username and email validation in loginUser

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -92,7 +92,7 @@ const registerUser = asyncHandler (async (req, res) => {
     )
 })
 
-const loginUser = asyncHandler (async (req, res) => {z
+const loginUser = asyncHandler (async (req, res) => {
     // get data from req body
     // username or email
     // find the user
@@ -102,9 +102,14 @@ const loginUser = asyncHandler (async (req, res) => {z
 
     const { email, username, password } = req.body
 
-    if (!username || !email) {
+    if (!username && !email) {
         throw new ApiError(400, "username or email is required")
     }
+
+    // Here is an alternative of above code based on logic discussed in video:
+    // if (!(username || email)) {
+    //     throw new ApiError(400, "username or email is required")
+    // }
 
     const user = await User.findOne({
         $or: [{ username }, { email }]
@@ -140,7 +145,7 @@ const loginUser = asyncHandler (async (req, res) => {z
                 accessToken,
                 refreshToken
             },
-            message="User logged in Successfully",
+            "User logged in Successfully",
         )
     )
 })
